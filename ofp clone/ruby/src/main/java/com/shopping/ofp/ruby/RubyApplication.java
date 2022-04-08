fb1e5a3248d66bfb0ac00e8fac46f8f77569b1c3
Nasaka Raja Vivek <rvnasaka23@gmail.com>
2022-04-08 23:57:59 +0530

    added starter proj details

---
+++ ofp clone/ruby/src/main/java/com/shopping/ofp/ruby/RubyApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.ofp.ruby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubyApplication.class, args);
+	}
+
+}
